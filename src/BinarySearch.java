8c29cf6c041e7c24d93d2a5c1093cfffebe69f85
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2023-08-15 06:57:27 +0500

    .

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -6,6 +6,7 @@ public class BinarySearch { /*бинарный поиск*/
         int low = 0;
         int mid, gues;
         int high = array.length - 1;
+        for (int n: array) System.out.print(n + " "); //Вывод входящего массива
 
         if (e < array[0] || e > array[array.length-1]) { /*проверка на вхождение в диапазон множества массива*/
             return "В диапазоне множества нет числа!";
@@ -15,18 +16,30 @@ public class BinarySearch { /*бинарный поиск*/
             mid = (low + high) / 2;
             gues = array[mid];
 
-            if (e == gues) return Integer.toString(mid); /* .....(array[mid] = e)..... */
+            if (e == gues) {
+                NumberSteps(array); /*вызов функции подсчета шагов*/
+                System.out.println("Элемент найден. Находится по индесу: " + Integer.toString(mid) + "\n");
+                return Integer.toString(mid); /* .....(array[mid] = e)..... */
+            }
             else if (e < gues) high = mid - 1;           /* ..e..(mid-1)|..... */
             else low = mid + 1;                          /* .....|(mid+1).e.. */
         }
         return "Не найден";
     }
 
-    public static void main(String[] args) {
-        int[] arrayBinarySearch1 = {1, 5, 5, 6, 7, 90, 456, 500, 501, 4543, 4544, 4550, 4643, 6643, 7777, 56423};
-        int[] arrayBinarySearch = {1, 2, 3, 4, 5, 6, 7, 7};
-        int e = 56421121;
+    static double NumberSteps (int[] array) {
+        System.out.println("\n Шагов " + Math.round(Math.log10(array.length)/Math.log10(2)) + "  от массива из " + array.length + " элементов");
+        return  Math.round(Math.log10(array.length)/Math.log10(2));
+    }
 
-        System.out.println(binarySearch(arrayBinarySearch1, e));
+    public static void main(String[] args) {
+        int[] arrayBinarySearch = {1, 5, 5, 6, 7, 90, 456, 500, 501, 4543, 4544, 4550, 4643, 6643, 7777, 56423};
+        int[] arrayBinarySearch1 = new int[240];
+        int e = 456;
+
+        binarySearch(arrayBinarySearch, e);
+        NumberSteps(arrayBinarySearch1);
+
+
     }
 }
