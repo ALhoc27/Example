7f0ac12d807af0184617537ad34130d1f08a341f
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2023-08-16 16:29:02 +0500

    .

--- src/Algorithm/Palindrome.java
+++ src/Algorithm/Palindrome.java
@@ -3,8 +3,7 @@ package Algorithm;
 import Algorithm.configO.EstimationAlgorithmComplexity;
 
 public class Palindrome {
-/*    В этом алгоритме цикл выполняется n/2 раз, где n — длина строки. Казалось бы, сложность работы алгоритма должна быть равна O(n/2).
-    Но, как и в прошлый раз, это не так. Алгоритмы со временем работы 2n, 10n, n/5, n/32 — все имеют сложность O(n),*/
+
     static boolean palindrome(String inWord) {
         int[] words = new int[inWord.length()];
         for (int i = 0; i < inWord.length() / 2; i++) {
@@ -18,8 +17,9 @@ public class Palindrome {
         return true;
     }
 
-/*    public static void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Палиндромом называется строка, которая одинаково читается слева направо и справа налево. АНА и ABBA — это палиндромы, а YES и NO — нет.");
+        System.out.println(" В этом алгоритме цикл выполняется n/2 раз, где n — длина строки. Казалось бы, сложность работы алгоритма должна быть равна O(n/2)\nНо, как и в прошлый раз, это не так. Алгоритмы со временем работы 2n, 10n, n/5, n/32 — все имеют сложность O(n).");
         System.out.println("");
         palindrome("ab");
         System.out.println();
@@ -30,5 +30,5 @@ public class Palindrome {
         palindrome("abcba");
         System.out.println();
         palindrome("1221");
-    }*/
+    }
 }
