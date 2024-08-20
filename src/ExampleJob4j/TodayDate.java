d2661841ba8373d6f204937ec84b1b61e8da124d
alexanderbabitskiy <alexbbtsk@gmail.com>
2024-08-20 21:10:02 +0500

    1

---
+++ src/ExampleJob4j/TodayDate.java
@@ -0,0 +1,13 @@
+package ExampleJob4j;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class TodayDate {
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String formattedDate = today.format(formatter);
+        System.out.println(formattedDate);
+    }
+}
