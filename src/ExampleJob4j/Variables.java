61ae9944adab8137952f7d2b46ae47de4b0440bb
alexanderbabitskiy <alexbbtsk@gmail.com>
2024-08-23 12:00:28 +0500

    1

---
+++ src/ExampleJob4j/Variables.java
@@ -0,0 +1,24 @@
+package ExampleJob4j;
+
+public class Variables {
+    public static void main(String[] args) {
+        byte mem = 1;
+        System.out.println(mem);
+        short size = 1;
+        System.out.println(size);
+        long money = 900500;
+        System.out.println(money);
+        float weight = 1.05F;
+        System.out.println(weight);
+        double distance = 100500.99;
+        System.out.println(distance);
+        boolean exists = true;
+        System.out.println(exists);
+        char gender = 'Y';
+        System.out.println(gender);
+        int age = 18;
+        float length = 78.0332f;
+        boolean baby = true;
+
+    }
+}
