8131bc655a6b60b96ff93d0cc05ec44f81daf600
Al <alexbbtsk@gmail.com>
2024-09-24 20:59:57 +0500

    1

--- src/main/java/other/BlockLambdaDemo2.java
+++ src/main/java/other/BlockLambdaDemo2.java
@@ -1,5 +1,7 @@
 package other;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class BlockLambdaDemo2 {
     public static void main(String[] args) {
         SomeFunc<String> stringFunc = (str) -> {
@@ -15,11 +17,17 @@ public class BlockLambdaDemo2 {
 
         SomeFunc<Integer> integerSomeFuncFactorial = n -> {
             int result = 1;
-            for (int i=1; i<=n; i++) {
+             for (int i=1; i<=n; i++) {
                 result *=i;
             }
             return result;
         };
         System.out.println(integerSomeFuncFactorial.func(4));
+        printL();
+    }
+    char ch = 65010;
+
+    static void printL() {
+        System.out.println(new BlockLambdaDemo2().ch);
     }
 }
